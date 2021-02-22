290fcecd348410e21eb40fa1e398444e35d346c7
Juan Camilo <lider.desarrollo@neuromedia.com.co>
2021-02-22 17:50:32 -0500

    fixed products

--- src/app/colombia/servicios/prueba-productos/prueba-productos.service.ts
+++ src/app/colombia/servicios/prueba-productos/prueba-productos.service.ts
@@ -18,14 +18,14 @@ export class PruebaProductosService {
   constructor(private httpC: HttpClient) {
     this.axios = require('axios').default;
 
-    this.axios.defaults.baseURL = 'http://localhost:8000/';
+    this.axios.defaults.baseURL = 'https://pruebasneuro.co/N-1062/api/';
     this.axios.defaults.withCredentials = true;
   }
 
   getListarProductos() {
 
     return new Promise(resolve => {
-      this.httpC.get('http://localhost:8000/api/producto/listado-productos' ).subscribe(data => {
+      this.httpC.get('https://pruebasneuro.co/N-1062/api/api/producto/listado-productos' ).subscribe(data => {
         resolve(data);
       }, error => {
         console.log(error);
@@ -49,7 +49,7 @@ export class PruebaProductosService {
 
     return new Promise((resolve, reject) => {
       this.axios.get('/sanctum/csrf-cookie').then(() => {
-        this.axios.post('http://localhost:8000/api/producto/listar-producto-filtros', data).then(response => {
+        this.axios.post('https://pruebasneuro.co/N-1062/api/api/producto/listar-producto-filtros', data).then(response => {
           resolve(response.data);
         }).catch(error => reject(error));
       }).catch(error => reject(error));
@@ -61,7 +61,7 @@ export class PruebaProductosService {
 
     return new Promise((resolve, reject) => {
       this.axios.get('/sanctum/csrf-cookie').then(() => {
-        this.axios.post('http://localhost:8000/api/producto/enviar-correo-cotizacion', data).then(response => {
+        this.axios.post('https://pruebasneuro.co/N-1062/api/api/producto/enviar-correo-cotizacion', data).then(response => {
           resolve(response.data);
         }).catch(error => reject(error));
       }).catch(error => reject(error));
@@ -71,7 +71,7 @@ export class PruebaProductosService {
 
   getlistarProductoUnico(id: number) {
     return new Promise(resolve => {
-      this.httpC.get('http://localhost:8000/api/producto/listar-unico-producto' + '/' + id).subscribe(data => {
+      this.httpC.get('https://pruebasneuro.co/N-1062/api/api/producto/listar-unico-producto' + '/' + id).subscribe(data => {
         resolve(data);
       }, error => {
         console.log(error);
--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,14 @@
 export var GLOBAL = {
-  auth: 'http://localhost/gersWordpress/wp-json/jwt-auth/v1/token',
-  base: 'http://localhost/gersWordpress/wp-json/acf/v3',
-  url: 'http://localhost/gersWordpress/wp-json/wp/v2',
-  menu: 'http://localhost/gersWordpress/wp-json/menus/v1'
+
+  // Localhost
+  // auth: 'http://localhost/gersWordpress/wp-json/jwt-auth/v1/token',
+  // base: 'http://localhost/gersWordpress/wp-json/acf/v3',
+  // url:  'http://localhost/gersWordpress/wp-json/wp/v2',
+  // menu: 'http://localhost/gersWordpress/wp-json/menus/v1'
+
+  // Producción
+  auth: 'https://pruebasneuro.co/N-1003backWordpress/wp-json/jwt-auth/v1/token',
+  base: 'https://pruebasneuro.co/N-1003backWordpress/wp-json/acf/v3',
+  url:  'https://pruebasneuro.co/N-1003backWordpress/wp-json/wp/v2',
+  menu: 'https://pruebasneuro.co/N-1003backWordpress/wp-json/menus/v1'
 };
