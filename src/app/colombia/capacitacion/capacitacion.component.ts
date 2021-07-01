5da74be883de17573abeb9d4461ac76a8bda6466
Nelson <nelsonzambrano23@gmail.com>
2021-07-01 15:15:10 -0500

    filtro capacitación y enviar correo inscripción

--- src/app/colombia/capacitacion/capacitacion.component.ts
+++ src/app/colombia/capacitacion/capacitacion.component.ts
@@ -17,6 +17,11 @@ export class CapacitacionComponent implements OnInit {
   eventos: any[] = [];
   capacitaciones: any[] = [];
   capacitacionesFiltro: any[] = [];
+  categorias: any[] = [];
+  filtrar: any[];
+  capacitacionesTemp: any[];
+  bandera: boolean = false;
+  eventosFiltro: any[];
 
 
 
@@ -24,17 +29,19 @@ export class CapacitacionComponent implements OnInit {
 
   ngOnInit(): void {
     this.asignarEventos();
+    this.getCategoriasFiltro();
     moment.locale('es');
   }
 
   asignarEventos() {
     this.capacitacionesS.getCapacitaciones().subscribe((resp: any[]) => {
       this.capacitaciones = resp;
+      console.log(this.capacitaciones);
       this.capacitaciones.forEach(element => {
         let inicio = new Date(element.acf.fecha_inicio);
         let fin = new Date(element.acf.fecha_fin);
-        let fechaInicio = inicio.getFullYear()+'-'+(inicio.getMonth()<10?'0'+(inicio.getMonth()+1):(inicio.getMonth()+1))+'-'+(inicio.getDate()<10?'0'+inicio.getDate():inicio.getDate())+'T'+element.acf.hora_inicio;
-        let fechaFin = fin.getFullYear()+'-'+(fin.getMonth()<10?'0'+(fin.getMonth()+1):(fin.getMonth()+1))+'-'+(fin.getDate()<10?'0'+fin.getDate():fin.getDate())+'T'+element.acf.hora_fin;
+        let fechaInicio = inicio.getFullYear() + '-' + (inicio.getMonth() < 10 ? '0' + (inicio.getMonth() + 1) : (inicio.getMonth() + 1)) + '-' + (inicio.getDate() < 10 ? '0' + inicio.getDate() : inicio.getDate()) + 'T' + element.acf.hora_inicio;
+        let fechaFin = fin.getFullYear() + '-' + (fin.getMonth() < 10 ? '0' + (fin.getMonth() + 1) : (fin.getMonth() + 1)) + '-' + (fin.getDate() < 10 ? '0' + fin.getDate() : fin.getDate()) + 'T' + element.acf.hora_fin;
         // console.log(fechaInicio);
         // console.log(fechaFin);
         this.eventos.push({
@@ -46,7 +53,8 @@ export class CapacitacionComponent implements OnInit {
           }
         })
       })
-    this.crearCronograma();
+      this.crearCronograma();
+      this.getCapacitaciones();
     })
   }
 
@@ -59,39 +67,148 @@ export class CapacitacionComponent implements OnInit {
     };
   }
 
-  reunionesDelDia(arg){
-    console.log(arg);
-    this.getReuniones(arg.dateStr)
-  }
-
-  changeView(calendar: string){
-    this.calendarComponent.getApi().changeView(calendar);
-  }
-
-  enviarInterna(capacitacion){
-    this.router.navigateByUrl(`/colombia/capacitaciones/${capacitacion.id}`);
-    // console.log(capacitacion);
-  }
-
-  getReuniones(date: string){
-    let fecha = new Date(date);
-    console.log(date);
-    let fechaSeleccionada = moment(date).format('MM/DD/YYYY');
-    // let fechaInicio = ((fecha.getMonth()+1)<10?'0'+(fecha.getMonth()+1):(fecha.getMonth()+1))+'/'+((fecha.getDate()+1)<10?'0'+(fecha.getDate()+1):(fecha.getDate()+1))+'/'+fecha.getFullYear();
-    // console.log(fechaInicio);
-    console.log(fechaSeleccionada);
+  getCapacitaciones(){
     let capacitacion = this.capacitaciones.filter(filtro => {
-      if(filtro.acf.fecha_inicio === fechaSeleccionada){
-        let fechaCapacitacion = moment(filtro.acf.fecha_inicio+' '+filtro.acf.hora_inicio).format('DD MMMM YYYY hh:mm:ss a');
+        let fechaCapacitacion = moment(filtro.acf.fecha_inicio + ' ' + filtro.acf.hora_inicio).format('DD MMMM YYYY hh:mm:ss a');
         filtro.fecha = fechaCapacitacion
         let categorias = [];
         filtro.categorias_capacitaciones.forEach(element => {
           this.capacitacionesS.getCategoriaCapacitacionesId(element).subscribe((resp: any) => {
             categorias.push(resp.name)
             filtro.categorias = categorias;
-          })  
+          })
         });
-        
+
+        return filtro;
+    });
+    capacitacion.sort(function(a,b){
+      if(a.acf.fecha_inicio < b.acf.fecha_inicio){
+        return 1;
+      }
+      if(a.acf.fecha_inicio > b.acf.fecha_inicio){
+        return -1;
+      }
+      return 0;
+    })
+    this.capacitacionesFiltro = capacitacion;
+    // console.log(this.capacitacionesFiltro);
+  }
+
+  getCategoriasFiltro() {
+    this.capacitacionesS.getCategoriaCapacitaciones().subscribe((resp : any) => {
+      // console.log(resp);
+      resp.forEach(element => {
+        element.bandera = false
+      });
+      this.categorias.push(...resp)
+      // console.log(this.categorias);
+    })
+  }
+
+  categoriasFiltro(){
+    this.filtrar = [];
+    this.eventosFiltro = [];
+    if(!this.capacitacionesTemp || this.capacitacionesTemp === this.capacitaciones){
+      this.capacitacionesTemp = this.capacitaciones;
+      // console.log(this.capacitacionesTemp);
+    }
+    // console.log(id);
+    // console.log(bandera);
+     this.capacitacionesTemp.forEach(element => {
+      this.categorias.forEach(filtro => {
+        element.categorias_capacitaciones.filter(filtroCate => {
+          if(filtro.bandera && filtro.id === filtroCate){
+              // console.log("Entro");
+              this.filtrar.push(element);
+              let inicio = new Date(element.acf.fecha_inicio);
+              let fin = new Date(element.acf.fecha_fin);
+              let fechaInicio = inicio.getFullYear() + '-' + (inicio.getMonth() < 10 ? '0' + (inicio.getMonth() + 1) : (inicio.getMonth() + 1)) + '-' + (inicio.getDate() < 10 ? '0' + inicio.getDate() : inicio.getDate()) + 'T' + element.acf.hora_inicio;
+              let fechaFin = fin.getFullYear() + '-' + (fin.getMonth() < 10 ? '0' + (fin.getMonth() + 1) : (fin.getMonth() + 1)) + '-' + (fin.getDate() < 10 ? '0' + fin.getDate() : fin.getDate()) + 'T' + element.acf.hora_fin;
+              this.eventosFiltro.push({
+                title: element.acf.tipo_de_evento, start: fechaInicio, end: fechaFin,
+                extendedProps: {
+                  id: element.id,
+                  fecha_Inicio: element.acf.fecha_inicio,
+                  fecha_FIn: element.acf.fecha_fin
+                }
+              })
+            }
+        });
+      })
+      
+    })
+    if (this.filtrar.length > 0) {
+      // console.log(this.filtrar);
+      this.capacitaciones = this.filtrar;
+      
+      console.log(this.eventosFiltro);
+      this.changeEvent(1);
+      
+      this.getCapacitaciones();
+    } else {
+      this.capacitaciones = this.capacitacionesTemp;
+      // this.calendarComponent.getApi().removeAllEvents();
+      console.log(this.eventos);
+      this.changeEvent(2);
+      // this.calendarComponent.getApi().addEvent(this.eventos);
+      this.getCapacitaciones();
+      // console.log(this.capacitaciones);
+    }
+
+  }
+
+  changeEvent(id: number){
+    if(id === 1){
+      this.calendarOptions = {
+        locale: LocalEs,
+        events: this.eventosFiltro,
+        dateClick: this.reunionesDelDia.bind(this),
+        eventClick: this.detalleReunion.bind(this)
+      };
+      // this.calendarComponent.getApi().removeAllEvents();
+      // this.calendarComponent.getApi().addEvent(this.eventosFiltro);
+      // this.calendarComponent.getApi().refetchEvents();
+    }else{
+      this.calendarOptions = {
+        locale: LocalEs,
+        events: this.eventos,
+        dateClick: this.reunionesDelDia.bind(this),
+        eventClick: this.detalleReunion.bind(this)
+      };
+    }
+  }
+
+  reunionesDelDia(arg) {
+    console.log(arg);
+    this.getReuniones(arg.dateStr)
+  }
+
+  changeView(calendar: string) {
+    this.calendarComponent.getApi().changeView(calendar);
+  }
+
+  enviarInterna(capacitacion) {
+    this.router.navigateByUrl(`/colombia/capacitaciones/${capacitacion.id}`);
+    // console.log(capacitacion);
+  }
+
+  getReuniones(date: string) {
+    let fecha = new Date(date);
+    console.log(date);
+    let fechaSeleccionada = moment(date).format('MM/DD/YYYY');
+    console.log(fechaSeleccionada);
+    let capacitacion = this.capacitaciones.filter(filtro => {
+      if (filtro.acf.fecha_inicio === fechaSeleccionada) {
+        let fechaCapacitacion = moment(filtro.acf.fecha_inicio + ' ' + filtro.acf.hora_inicio).format('DD MMMM YYYY hh:mm:ss a');
+        filtro.fecha = fechaCapacitacion
+        let categorias = [];
+        filtro.categorias_capacitaciones.forEach(element => {
+          this.capacitacionesS.getCategoriaCapacitacionesId(element).subscribe((resp: any) => {
+            categorias.push(resp.name)
+            filtro.categorias = categorias;
+          })
+        });
+
         return filtro;
       }
     });
--- src/app/colombia/interna-capacitacion/interna-capacitacion.component.ts
+++ src/app/colombia/interna-capacitacion/interna-capacitacion.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import * as moment from 'moment';
 import { CapacitacionesService } from 'src/app/services/capacitaciones.service';
+import Swal from 'sweetalert2';
+declare var $: any;
 
 @Component({
   selector: 'app-interna-capacitacion',
@@ -10,10 +12,18 @@ import { CapacitacionesService } from 'src/app/services/capacitaciones.service';
 })
 export class InternaCapacitacionComponent implements OnInit {
 
+  usuario: any;
   id: string;
   capacitacion: any;
   constructor(private activedRouter: ActivatedRoute, private capacitacionesS: CapacitacionesService) { 
     this.id = this.activedRouter.snapshot.paramMap.get('id');
+    this.usuario = {
+      nombres: '',
+      apellidos:'',
+      email: '',
+      telefono:'',
+      acepto:''
+    };
   }
 
   ngOnInit(): void {
@@ -28,8 +38,39 @@ export class InternaCapacitacionComponent implements OnInit {
           this.capacitacion.categorias = categorias;
         })  
       });
-      
     })
   }
+  enviarInscripcion(){
+    $("#wrapper").toggleClass("toggled");
+    $('.overlaytrabaja').addClass('active');
+  }
 
+  public cierraInscripcion() {
+    $('.overlaytrabaja').removeClass('active');
+    $("#wrapper").toggleClass("toggled");
+  }
+  formTrabajeNosotros(form){
+    $.ajax({
+      url: 'https://pruebasneuro.co/N-1003backWordpress/wp-content/themes/gers/inscripcion.php',
+      type: 'POST',
+      data: JSON.stringify(this.usuario),
+      dataType:"json",
+      success: function(data) {
+        console.log(data);
+      }, error: function(error){
+        console.log(error);
+        if(error.status === 200){
+          Swal.fire({
+            icon: 'success',
+            title: 'Gracias por regalarnos tus datos. Nos comunicaremos contigo.',
+            showConfirmButton: true
+          }); 
+          //console.log(error);
+        form.reset();
+        } else {
+          Swal.fire('Oops...', 'Algo pasó. Corrige los errores, por favor!', 'error')
+        }
+      }
+    });
+   }
 }
