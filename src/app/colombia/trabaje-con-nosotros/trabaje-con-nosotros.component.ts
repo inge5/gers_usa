c6a9c6f489a3b5c1a84160f392a704d3f0da4e8e
Danny Valencia <inge3neuromedia@gmail.com>
2021-07-14 10:58:34 -0500

    Pruebas nuevas de correos vacantes general

--- src/app/colombia/trabaje-con-nosotros/trabaje-con-nosotros.component.ts
+++ src/app/colombia/trabaje-con-nosotros/trabaje-con-nosotros.component.ts
@@ -13,22 +13,23 @@ declare var $ : any;
   styleUrls: ['./trabaje-con-nosotros.component.css']
 })
 export class TrabajeConNosotrosComponent implements OnInit {
-  public usuario: any;
+  //public usuario: any;
+  public usuario = {
+    nombres: '',
+    apellidos:'',
+    email: '',
+    telefono:'',
+    ubicacion:'Colombia',
+    categoria:'',
+    archivo: null,
+    acepto:'',
+  };
 
   loader = true;
   vacantes_data:any[] = [];
 
   constructor(private _router:Router, private _vacantesservice:VacantesService) {
-    this.usuario = {
-      nombres: '',
-      apellidos:'',
-      email: '',
-      telefono:'',
-      ubicacion:'Colombia',
-      categoria:'',
-      archivo: '',
-      acepto:'',
-    };
+    //this.usuario.ubicacion = 'Colombia';
    }
 
   ngOnInit(): void {
@@ -53,16 +54,37 @@ export class TrabajeConNosotrosComponent implements OnInit {
     this._router.navigate(['/colombia/vacantes', slug]);
   }
 
+  changeFile(file:File){
+    this.usuario.archivo = file[0];
+    console.log(this.usuario.archivo);
+  }
+
   formTrabajeNosotros(form){
+    console.log(this.usuario.archivo);
+    var paqueteDeDatos = new FormData();
+    paqueteDeDatos.append('archivo', this.usuario.archivo);
+    paqueteDeDatos.append('nombres', this.usuario.nombres);
+    paqueteDeDatos.append('apellidos', this.usuario.apellidos);
+    paqueteDeDatos.append('email', this.usuario.email);
+    paqueteDeDatos.append('telefono', this.usuario.telefono);
+    paqueteDeDatos.append('ubicacion', this.usuario.ubicacion);
+    paqueteDeDatos.append('categoria', this.usuario.categoria);
+    paqueteDeDatos.append('acepto', this.usuario.acepto);
+    var destino = "https://pruebasneuro.co/N-1003backWordpress/wp-content/themes/gers/formulario-vacantes-general/form-vacantes-general.php"; // El script que va a recibir los campos de formulario.
+					/* Se envia el paquete de datos por ajax. */
     $.ajax({
-      url: 'https://pruebasneuro.co/N-1003backWordpress/wp-content/themes/gers/formulario-vacantes-general/form-vacantes-general.php',
+      url: destino,
       type: 'POST',
+      /*
       data: JSON.stringify(this.usuario),
       dataType:"json",
+      */
+      contentType: false,
+      data: paqueteDeDatos, // Al atributo data se le asigna el objeto FormData.
+      processData: false,
+      cache: false, 
       success: function(data) {
-        
-      }, error: function(error){
-        if(error.status === 200){
+        if(data.status === 200){
           Swal.fire({
             icon: 'success',
             title: 'Gracias por regalarnos tus datos. Nos comunicaremos contigo.',
@@ -73,6 +95,8 @@ export class TrabajeConNosotrosComponent implements OnInit {
         } else {
           Swal.fire('Oops...', 'Algo pasó. Corrige los errores, por favor!', 'error')
         }
+      }, error: function(error){
+        
       }
     });
    }
