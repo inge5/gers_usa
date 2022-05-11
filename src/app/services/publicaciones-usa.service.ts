b38ca5bff9eea3e11b6b664bcb4fbbc3007300e1
Nelson Zambrano <inge5neuromedia@gmail.com>
2022-05-11 10:09:21 -0500

    insights y direccion texas

--- src/app/services/publicaciones-usa.service.ts
+++ src/app/services/publicaciones-usa.service.ts
@@ -6,31 +6,30 @@ import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PublicacionesUsaService {
   public url: string;
 
-  constructor(private _http: HttpClient, private _router:Router) { 
+  constructor(private _http: HttpClient, private _router: Router) {
     this.url = environment.urlUsa;
   }
 
-  getPublicaciones(): Observable<any>{
-    return this._http.get(`${this.url}/publicaciones`);
+  getPublicaciones(): Observable<any> {
+    return this._http.get(`${this.url}/publicaciones?per_page=100`);
   }
 
-  getPublicacion(slug: string): Observable<any>{
-    return this._http.get(`${this.url}/publicaciones/?slug=${slug}/`, {
-    }).pipe(
-        map(res => res),
-        catchError(err =>{
-          switch(err.status){
-            case 404:
-              this._router.navigate(['/404']);
-              break;
-          }
-          return throwError('Error en el servicio');
-        })
+  getPublicacion(slug: string): Observable<any> {
+    return this._http.get(`${this.url}/publicaciones/?slug=${slug}/`, {}).pipe(
+      map((res) => res),
+      catchError((err) => {
+        switch (err.status) {
+          case 404:
+            this._router.navigate(['/404']);
+            break;
+        }
+        return throwError('Error en el servicio');
+      })
     );
   }
 }
