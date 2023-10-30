6583d6e8b2c114937d38d20c1805b61b8966064f
Nelson Zambrano <nelsonzambrano23@gmail.com>
2023-10-30 04:07:22 -0500

    ajustes y cambios de correo

--- src/app/usa/menu-vertical-usa/menu-vertical-usa.component.ts
+++ src/app/usa/menu-vertical-usa/menu-vertical-usa.component.ts
@@ -2,33 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { MenusUsaService } from 'src/app/services/menus-usa.service';
 declare var $: any;
 
-
 @Component({
   selector: 'app-menu-vertical-usa',
   templateUrl: './menu-vertical-usa.component.html',
-  styleUrls: ['./menu-vertical-usa.component.css']
+  styleUrls: ['./menu-vertical-usa.component.css'],
 })
 export class MenuVerticalUsaComponent implements OnInit {
   representaciones: any;
   servicios: any;
 
-  constructor(private _menusService:MenusUsaService) { }
+  constructor(private _menusService: MenusUsaService) {}
 
   ngOnInit(): void {
     this.getMenuPrincipal();
   }
 
-  getMenuPrincipal(){
-    this._menusService.getMenuPrincipal()
-    .subscribe((res:any) => {
+  getMenuPrincipal() {
+    this._menusService.getMenuPrincipal().subscribe((res: any) => {
       this.servicios = res.items[0];
       this.representaciones = res.items[1];
-    });  
+    });
   }
 
-  cerrarMenuVertical(){
+  cerrarMenuVertical() {
     $('#menuverticalUsa').removeClass('switch-cotizador');
     $('.overviwe').removeClass('open-overviwe');
   }
-
 }
