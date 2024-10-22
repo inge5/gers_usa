b4de646966218e9d6ff8ce9c871cac75113443d2
= <=>
2024-10-22 07:46:13 -0500

    NZ - add logo neta footer

--- src/app/usa/footer-usa/footer-usa.component.ts
+++ src/app/usa/footer-usa/footer-usa.component.ts
@@ -4,34 +4,32 @@ import { MenusUsaService } from '../../services/menus-usa.service';
 @Component({
   selector: 'app-footer-usa',
   templateUrl: './footer-usa.component.html',
-  styleUrls: ['./footer-usa.component.css']
+  styleUrls: ['./footer-usa.component.css'],
 })
 export class FooterUsaComponent implements OnInit {
-
   aboutUs: any;
   contacto: any;
   redesSociales: any;
-  constructor(private menusUsaService: MenusUsaService) { }
+  constructor(private menusUsaService: MenusUsaService) {}
 
   ngOnInit(): void {
     this.menuFooterAbout();
     this.menuFooterContact();
     this.menuFooterSocial();
   }
-  menuFooterAbout(){
-    this.menusUsaService.getFooterAbout().subscribe(resp => {
+  menuFooterAbout() {
+    this.menusUsaService.getFooterAbout().subscribe((resp) => {
       this.aboutUs = resp.items;
     });
   }
-  menuFooterContact(){
-    this.menusUsaService.getFooterContact().subscribe(resp => {
+  menuFooterContact() {
+    this.menusUsaService.getFooterContact().subscribe((resp) => {
       this.contacto = resp.items;
     });
   }
-  menuFooterSocial(){
-    this.menusUsaService.getFooterSocial().subscribe(resp => {
+  menuFooterSocial() {
+    this.menusUsaService.getFooterSocial().subscribe((resp) => {
       this.redesSociales = resp.items;
     });
   }
-
 }
